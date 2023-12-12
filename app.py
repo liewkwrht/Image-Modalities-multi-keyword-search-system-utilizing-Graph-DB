7c5bbd9b67e2ac3de04a28c419d57029c7b0ca42
kavinrath <kwrhtliew@gmail.com>
2023-12-12 15:02:38 +0700

    multi symtoms complete , pattern search

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 CORS(app)
 logging.basicConfig(level=logging.INFO)
 
-# Instantiate the Neo4jConnector with the proper configuration variables
 neo4j_connector = Neo4jConnector(NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD)
 
 @app.route('/api/search', methods=['POST'])
@@ -17,30 +16,32 @@ def search():
     data = request.json
     body_part_name = data.get('bodypart')
     disease_name = data.get('disease')
-    symptom_name = data.get('symptom')
-    target_classes = data.get('targetClasses')  # Assuming 'targetClasses' is sent in the request body
+    symptom_name = data.get('symptom', [])
+    target_classes = data.get('targetClasses')
 
     try:
         start_time = time.time()
         serializable_result = neo4j_connector.search_nodes_by_name(body_part_name, symptom_name, disease_name, target_classes)
         execution_time = time.time() - start_time
 
-        # Convert execution time to milliseconds
-        execution_time_ms = execution_time * 1000
+        unique_uids = set()
+        for path_data in serializable_result:
+            for node in path_data['nodes']:
+                if 'uid' in node['properties']:
+                    unique_uids.add(node['properties']['uid'])
 
-        if not serializable_result:
-            return jsonify({'execution_time_ms': execution_time_ms, 'data': []}), 204
+        uid_count = len(unique_uids)
 
         response = {
-            "execution time": execution_time_ms,  # Reporting time in milliseconds
-            "data": serializable_result
+            "execution time (s)": execution_time,
+            "data": serializable_result,
+            "unique_uid_count": uid_count  
         }
 
         return jsonify(response), 200
     except Exception as e:
         app.logger.error(f"An error occurred: {e}")
         return jsonify({'error': str(e)}), 500
-
 
 @app.teardown_appcontext
 def close_connection(exception=None):
--- neo4j_script.py
+++ neo4j_script.py
@@ -32,13 +32,14 @@ class Neo4jConnector:
     
     def search_nodes_by_name(self, bodyPartName, symptomName, diseaseName, targetClasses=None):
         with self.get_session() as session:
-            # Define input names
-            inputNames = [name for name in [bodyPartName, symptomName, diseaseName] if name]
 
-            # Start building your query string
+            inputNames = [bodyPartName] + symptomName + [diseaseName] 
+            inputNames = [name for name in inputNames if name]
+
+            
             query = "WITH $inputNames AS inputNames "
 
-            # Add target classes to the query if provided
+            
             if targetClasses:
                 query += ", $targetClasses AS targetClasses "
                 query += (
@@ -51,24 +52,22 @@ class Neo4jConnector:
                     "WHERE node.name IN inputNames "
                 )
 
-            # Finish off your query
             query += "RETURN DISTINCT p;"
-
-            # Create parameters dictionary
             parameters = {'inputNames': inputNames}
             
-            # Add targetClasses to parameters if provided
+            
             if targetClasses:
                 parameters['targetClasses'] = targetClasses
 
-            # Run the query
+    
             result = session.run(query, parameters=parameters)
             records = list(result)
 
-        # Process the records to serialize the paths
+        
         serializable_result = []
         for record in records:
             path = record['p']
+            
             # Extract nodes and relationships from the path
             nodes = [{'id': node.id, 'labels': list(node.labels), 'properties': dict(node)} for node in path.nodes]
             relationships = [{'id': rel.id, 'type': rel.type, 'properties': dict(rel)} for rel in path.relationships]
